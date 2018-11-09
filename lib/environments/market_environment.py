ed66af14f21020231ace950bb58dcf0b92ebb823
muyuan.zhai <muyuan.zhai@datayes.com>
2018-11-09 20:27:07 +0800

    Myron: add environments.

---
+++ lib/environments/market_environment.py
@@ -0,0 +1,127 @@
+# -*- coding: UTF-8 -*-
+# **********************************************************************************#
+#     File: Market environment.
+# **********************************************************************************#
+from gym import Env
+from utils.exceptions import Exceptions
+
+
+class MarketEnvironment(Env):
+
+    def __init__(self, **kwargs):
+        """
+        Initialize your environment parameter here.
+
+        Args:
+            **kwargs(**dict): key-word arguments when you initialize your environment.
+        """
+        valid_parameters = {
+            'metadata',
+            'reward_range',
+            'spec',
+            'action_space',
+            'observation_space'
+        }
+        if not set(kwargs).issubset(set(valid_parameters)):
+            raise Exceptions.INVALID_INITIALIZE_PARAMETERS
+        self.__dict__.update(kwargs)
+
+    def step(self, action):
+        """Run one timestep of the environment's dynamics. When end of
+        episode is reached, you are responsible for calling `reset()`
+        to reset this environment's state.
+
+        Accepts an action and returns a tuple (observation, reward, done, info).
+
+        Args:
+            action (object): an action provided by the environment
+
+        Returns:
+            observation (object): agent's observation of the current environment
+            reward (float) : amount of reward returned after previous action
+            done (boolean): whether the episode has ended, in which case further step() calls will return undefined results
+            info (dict): contains auxiliary diagnostic information (helpful for debugging, and sometimes learning)
+        """
+        raise NotImplementedError
+
+    def reset(self):
+        """Resets the state of the environment and returns an initial observation.
+
+        Returns: observation (object): the initial observation of the
+            space.
+        """
+        raise NotImplementedError
+
+    def render(self, mode='human'):
+        """Renders the environment.
+
+        The set of supported modes varies per environment. (And some
+        environments do not support rendering at all.) By convention,
+        if mode is:
+
+        - human: render to the current display or terminal and
+          return nothing. Usually for human consumption.
+        - rgb_array: Return an numpy.ndarray with shape (x, y, 3),
+          representing RGB values for an x-by-y pixel image, suitable
+          for turning into a video.
+        - ansi: Return a string (str) or StringIO.StringIO containing a
+          terminal-style text representation. The text can include newlines
+          and ANSI escape sequences (e.g. for colors).
+
+        Note:
+            Make sure that your class's metadata 'render.modes' key includes
+              the list of supported modes. It's recommended to call super()
+              in implementations to use the functionality of this method.
+
+        Args:
+            mode (str): the mode to render with
+            close (bool): close all open renderings
+
+        Example:
+
+        class MyEnv(Env):
+            metadata = {'render.modes': ['human', 'rgb_array']}
+
+            def render(self, mode='human'):
+                if mode == 'rgb_array':
+                    return np.array(...) # return RGB frame suitable for video
+                elif mode is 'human':
+                    ... # pop up a window and render
+                else:
+                    super(MyEnv, self).render(mode=mode) # just raise an exception
+        """
+        raise NotImplementedError
+
+    def close(self):
+        """Override _close in your subclass to perform any necessary cleanup.
+
+        Environments will automatically close() themselves when
+        garbage collected or when the program exits.
+        """
+        return
+
+    def seed(self, seed=None):
+        """Sets the seed for this env's random number generator(s).
+
+        Note:
+            Some environments use multiple pseudorandom number generators.
+            We want to capture all such seeds used in order to ensure that
+            there aren't accidental correlations between multiple generators.
+
+        Returns:
+            list<bigint>: Returns the list of seeds used in this env's random
+              number generators. The first value in the list should be the
+              "main" seed, or the value which a reproducer should pass to
+              'seed'. Often, the main seed equals the provided 'seed', but
+              this won't be true if seed=None, for example.
+        """
+        return
+
+    @property
+    def unwrapped(self):
+        """Completely unwrap this env.
+
+        Returns:
+            gym.Env: The base non-wrapped gym.Env instance
+        """
+        return self
---
+++ utils/exceptions.py
@@ -0,0 +1,48 @@
+# -*- coding: UTF-8 -*-
+# **********************************************************************************#
+#     File:
+# **********************************************************************************#
+import traceback
+
+
+error_wrapper = (lambda code, message: {'code': code, 'data': message, 'msg': message})
+
+
+def deal_with_exception(func):
+    """
+    Deal with exception.
+    """
+    def _decorator(obj, *args, **kwargs):
+        try:
+            response = func(obj, *args, **kwargs)
+        except tuple(Exceptions.error_types()), error_code:
+            response = error_code.args[0]
+        except:
+            response = error_wrapper(500, 'Exception unknown.'.format(func.func_name))
+        return response
+    return _decorator
+
+
+class EnvironmentsException(Exception):
+    """
+    Exception in module environments.
+    """
+    pass
+
+
+class Exceptions(object):
+    """
+    Enumerate exceptions.
+    """
+    INVALID_INITIALIZE_PARAMETERS = EnvironmentsException(error_wrapper(500, 'You have invalid input parameters'
+                                                                             ' when you initialize your environment.'))
+
+    @classmethod
+    def enumerates(cls):
+        return [value for attr, value in cls.__dict__.iteritems()]
+
+    @classmethod
+    def error_types(cls):
+        return tuple([
+            EnvironmentsException
+        ])
