8c8e9b738235aa994fc6db1a8ba1b8ec3d4c5d7d
muyuan.zhai <zhai14285574>
2019-02-12 16:29:04 +0800

    Myron: fix code.

--- tests/test_data/test_database_api.py
+++ tests/test_data/test_database_api.py
@@ -13,7 +13,7 @@ print(get_direct_trading_day(datetime(2015, 1, 1), step=0, forward=True))
 print(get_direct_trading_day(datetime(2015, 1, 1), step=1, forward=True))
 print(get_direct_trading_day(datetime(2015, 1, 1), step=1, forward=False))
 print(load_futures_base_info(['RB1810']))
-print (load_futures_main_contract(contract_objects=['RB', 'AG'], start='20180401', end='20180502'))
+print(load_futures_main_contract(contract_objects=['RB', 'AG'], start='20180401', end='20180502'))
 daily_data = load_futures_daily_data(['RB1810', 'RM809'],
                                      get_trading_days('20180301', '20180401'),
                                      attributes=['closePrice', 'turnoverValue'])
