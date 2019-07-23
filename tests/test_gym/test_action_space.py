74b0a052335dcd5127bc9ad90d6f8cb71759af62
muyuan.zhai <zhai14285574>
2019-07-23 17:21:58 +0800

    Myron: add test TradingActionSpace.

--- tests/test_gym/test_action_space.py
+++ tests/test_gym/test_action_space.py
@@ -3,7 +3,11 @@
 #     File:  Test action space.
 # **********************************************************************************#
 from unittest import TestCase
-from brain.trade_env.action_space import LongShortSpace
+from brain.trade_env.action_space import (
+    LongShortSpace,
+    TradingActionSpace
+)
+from brain.trade_env.base import TradingAction
 
 
 class TestActionSpace(TestCase):
@@ -17,3 +21,15 @@ class TestActionSpace(TestCase):
         print(action_space.contains(1))
         print(action_space.contains(-1))
         print(action_space.contains(0))
+
+    def test_trading_action_space(self):
+        """
+        Test trading action space.
+        """
+        action_space = TradingActionSpace()
+        print(action_space.sample())
+        print(action_space.contains(TradingAction.BUY))
+        print(action_space.contains(TradingAction.SELL))
+        print(action_space.contains(TradingAction.SHORT))
+        print(action_space.contains(TradingAction.COVER))
+        print(action_space.contains(TradingAction.FAIR))
